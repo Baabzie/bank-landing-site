2f9d5860cc72369afa811c378bdb3141489c79d7
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2024-10-08 18:44:56 +0200

    "latest article" looking ok

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -85,24 +85,57 @@ const Home: React.FC = () => {
         <div className={styles["third-row-lower"]}>
           <div>
             <img src="/images/image-currency.jpg" alt="A picture of money." />
+            <div className={styles["article-text-div"]}>
+              <p>By Claire Robinson</p>
+              <h3>Receive money in any currency with no fees</h3>
+              <p>
+                The world is getting smaller and we’re becoming more mobile. So
+                why should you be forced to only receive money in a single …
+              </p>
+            </div>
           </div>
           <div>
             <img
               src="/images/image-restaurant.jpg"
               alt="A picture of a table with food."
             />
+            <div className={styles["article-text-div"]}>
+              <p>By Wilson Hutton</p>
+              <h3>Treat yourself without worrying about money</h3>
+              <p>
+                Our simple budgeting feature allows you to separate out your
+                spending and set realistic limits each month. That means you …
+              </p>
+            </div>
           </div>
           <div>
             <img
               src="/images/image-plane.jpg"
               alt="A picture of an airplane wing."
             />
+            <div className={styles["article-text-div"]}>
+              <p>By Wilson Hutton</p>
+              <h3>Take your Easybank card wherever you go</h3>
+              <p>
+                We want you to enjoy your travels. This is why we don’t charge
+                any fees on purchases while you’re abroad. We’ll even show you …
+              </p>
+            </div>
           </div>
           <div>
             <img
               src="/images/image-confetti.jpg"
               alt="A picture of confetti flying in the air."
             />
+            <div className={styles["article-text-div"]}>
+              <p>By Claire Robinson</p>
+              <h3>Our invite-only Beta accounts are now live!</h3>
+              <p>
+                After a lot of hard work by the whole team, we’re excited to
+                launch our closed beta. It’s easy to request an invite through
+                the site ...
+              </p>
+            </div>
           </div>
         </div>
       </div>
