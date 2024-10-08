c3a35c5a5a8e14d86e734bbce638c1590838759c
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2024-10-08 18:57:14 +0200

    start of footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,8 +3,7 @@ import styles from "./Footer.module.scss";
 const Footer: React.FC = () => {
   return (
     <footer className={styles["footer"]}>
-      {/* <h1>Tobias Olausson</h1>
-      <p>Front-end Developer</p> */}
+      <img src="/images/logo.svg" alt="Easybanks logo" />
     </footer>
   );
 };
