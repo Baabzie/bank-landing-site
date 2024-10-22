663a99c2eeddae63eea63976ad0cefc3d2604d84
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2024-10-22 22:05:54 +0200

    some removal in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  basePath: isProd ? "/bank-landing-site" : "",
-  assetPrefix: isProd ? "/bank-landing-site/" : "",
   reactStrictMode: true,
   output: "export",
   distDir: "out",
