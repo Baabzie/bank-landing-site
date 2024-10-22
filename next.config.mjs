fba6c628638e1d89dbfba41fcf2a4a2287736a79
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2024-10-22 21:14:18 +0200

    updated next.config to fix img

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  basePath: isProd ? "/bank-landing-site" : "",
   reactStrictMode: true,
 };
 
